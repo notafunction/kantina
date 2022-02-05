eb8844c8ac4cfa88c3f5a80436b018b47b6af48c
Matt Latham <github@mattlatham.dev>
2022-02-04 23:27:40 -0800

    Integrate Firebase App Check

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,8 +3,10 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/database'
 import 'firebase/compat/auth'
 import 'firebase/compat/storage'
+import 'firebase/compat/app-check'
 
 export const firebaseConfig = {
+  appId: '1:898560228891:web:ce19c5e5cc965ec0',
   apiKey: 'AIzaSyCsW6mSWMZSLeUrJ6kHfQVhwDCTI9UGkCc',
   authDomain: 'kantina-b8628.firebaseapp.com',
   databaseURL: 'https://kantina-b8628.firebaseio.com',
@@ -14,5 +16,6 @@ export const firebaseConfig = {
 }
 
 firebase.initializeApp(firebaseConfig)
+firebase.appCheck().activate('6Lc9LFseAAAAAPlnOmHB8kaCnM3hLagkbr9v1YN3', true)
 
 export default firebase
