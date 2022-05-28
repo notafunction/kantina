1b11cb53b45d8db6c107e9ce2d1807cea2235720
Matt Latham <matt@mattlatham.dev>
2022-05-28 06:11:19 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {},
+}
