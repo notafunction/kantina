6f169d397e58555debdc3cdded2f6cb78728380a
Matt Latham <matt@mattlatham.dev>
2022-07-26 20:09:41 -0700

    move firebase config to PRC

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,17 @@ export default defineNuxtConfig({
       tracesSampleRate: 1.0,
       debug: false,
     },
+    firebase: {
+      config: {
+        apiKey: 'AIzaSyCsW6mSWMZSLeUrJ6kHfQVhwDCTI9UGkCc',
+        authDomain: 'kantina-b8628.firebaseapp.com',
+        databaseURL: 'https://kantina-b8628.firebaseio.com',
+        projectId: 'kantina-b8628',
+        storageBucket: 'kantina-b8628.appspot.com',
+        messagingSenderId: '898560228891',
+        appId: '1:898560228891:web:ce19c5e5cc965ec0',
+      },
+    },
   },
 
   modules: ['@unocss/nuxt'],
--- plugins/firebase.client.js
+++ plugins/firebase.client.js
@@ -1,21 +1,9 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
 import { initUser } from '~/composables/useFirebaseAuth'
 import { useFirebaseUser } from '~/composables/useStates'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCsW6mSWMZSLeUrJ6kHfQVhwDCTI9UGkCc',
-  authDomain: 'kantina-b8628.firebaseapp.com',
-  databaseURL: 'https://kantina-b8628.firebaseio.com',
-  projectId: 'kantina-b8628',
-  storageBucket: 'kantina-b8628.appspot.com',
-  messagingSenderId: '898560228891',
-  appId: '1:898560228891:web:ce19c5e5cc965ec0',
-}
-
 export default defineNuxtPlugin((nuxtApp) => {
-  const app = initializeApp(firebaseConfig)
-  const auth = getAuth()
+  const app = initializeApp(nuxtApp.$config.firebase.config)
   const firebaseUser = useFirebaseUser()
 
   initUser()
