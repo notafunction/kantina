bd887f3de2d2ea273cdd39959143d5b6c23428cd
Matt Latham <matt@greyskymedia.com>
2022-09-02 16:03:15 -0700

    add drag

--- src/containers/Item/Item.js
+++ src/containers/Item/Item.js
@@ -5,12 +5,21 @@ import ItemToolbar from './components/ItemToolbar'
 import { useDatabase, useDatabaseObjectData } from 'reactfire'
 import { ref } from 'firebase/database'
 import { Spin } from 'antd'
+import { useDrag } from 'react-dnd'
+import { DndItemTypes } from '../Dnd/Dnd'
 
 const Item = (props) => {
   const db = useDatabase()
   const item = useDatabaseObjectData(ref(db, `items/${props.item.id}`), {
     idField: 'id'
   })
+
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: DndItemTypes.ITEM,
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging()
+    })
+  }))
 
   if (item.status === 'loading') return <Spin />
 
