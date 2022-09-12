8cc6ef65e75a7131c6d8bc62acacadcdc809389d
Matt Latham <matt@mattlatham.dev>
2022-09-12 01:10:23 -0700

    fix: add create option to nav

--- src/components/User/UserMenu.jsx
+++ src/components/User/UserMenu.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { Avatar, Dropdown, Menu } from 'antd'
-import { LogoutOutlined, SettingOutlined } from '@ant-design/icons'
+import { Avatar, Button, Dropdown, Menu, Space } from 'antd'
+import { LogoutOutlined, PlusOutlined, SettingOutlined } from '@ant-design/icons'
 import { useAuth } from 'reactfire'
 import UserSettingsModal from '../../containers/User/UserSettingsModal'
+import CreateBoardModal from '../../containers/Board/components/CreateBoardModal'
 function UserMenu(props) {
   const auth = useAuth()
 
-  const [UserSettingsModalVisible, setUserSettingsModalVisible] = React.useState(false)
+  const [userSettingsModalVisible, setUserSettingsModalVisible] = useState(false)
+  const [boardCreateModalVisible, setBoardCreateModalVisible] = useState(false)
 
   const handleMenuClick = (event) => {
     switch (event.key) {
@@ -35,7 +37,14 @@ function UserMenu(props) {
   )
 
   return (
-    <>
+    <Space>
+      <Button
+        icon={<PlusOutlined />}
+        onClick={() => setBoardCreateModalVisible(true)}
+        type="primary">
+        Create Board
+      </Button>
+
       <Dropdown overlay={menu} trigger={['click']}>
         <Avatar
           shape="square"
@@ -46,12 +55,17 @@ function UserMenu(props) {
         />
       </Dropdown>
 
+      <CreateBoardModal
+        visible={boardCreateModalVisible}
+        close={() => setBoardCreateModalVisible(false)}
+      />
+
       <UserSettingsModal
         user={props.user}
-        visible={UserSettingsModalVisible}
+        visible={userSettingsModalVisible}
         close={() => setUserSettingsModalVisible(false)}
       />
-    </>
+    </Space>
   )
 }
 
