8af00be72b31ad681b5420db0d8b11787e4d5c1f
Matt Latham <matt@mattlatham.dev>
2022-11-07 15:19:40 -0800

    chore: test env variable

--- api/hello.ts
+++ api/hello.ts
@@ -1,11 +1,13 @@
 import { initializeApp } from 'firebase-admin/app'
+import { getDatabase } from 'firebase-admin/database'
 
 export default function handler(req, res) {
   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)
 
   const app = initializeApp(serviceAccount)
+  const db = getDatabase(app)
 
-  console.log(app)
+  console.log(db)
 
   const { name = 'World' } = req.query;
   return res.send(`Hello ${name}!`);
