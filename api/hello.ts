e213ac3aa9beb2af19969b416876e04e7767c350
Matt Latham <matt@mattlatham.dev>
2022-11-07 15:12:09 -0800

    chore: test env variable

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  console.log(req, res)
+  console.log(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)
   const { name = 'World' } = req.query;
   return res.send(`Hello ${name}!`);
 }
