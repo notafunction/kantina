119754ad141d094823020dca0dcb5d00fc2cc360
Matt Latham <matt@mattlatham.dev>
2022-11-07 16:51:26 -0800

    chore: test

--- api/hello.ts
+++ api/hello.ts
@@ -1,14 +1,7 @@
-import { initializeApp } from 'firebase-admin/app'
-import { getDatabase } from 'firebase-admin/database'
+import { db } from '../lib/firebase'
 
 export default function handler(req, res) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)
-
-  const app = initializeApp(serviceAccount)
-  const db = getDatabase(app)
-
   console.log(db)
-
   const { name = 'World' } = req.query;
   return res.send(`Hello ${name}!`);
 }
