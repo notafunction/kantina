1262a61f4e773f5d6acc0c64c662eb14b473b93b
Matt Latham <matt@mattlatham.dev>
2022-11-08 17:22:29 -0800

    chore: add '~' alias

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [reactPlugin(), macrosPlugin()],
   resolve: {
     alias: {
+      '~': path.resolve(__dirname, '.'),
       '@': path.resolve(__dirname, './src')
     }
   }
